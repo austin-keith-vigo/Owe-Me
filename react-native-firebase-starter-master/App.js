ac7b5a6bbd45d0ad078b0c36f2e5d6780da32b15
Austin Vigo <avigo@uci.edu>
2019-09-04 15:41:41 -0700

    wrote code
    wrote code for the login screen

--- react-native-firebase-starter-master/App.js
+++ react-native-firebase-starter-master/App.js
@@ -4,33 +4,15 @@ import {
   View
 } from 'react-native';
 import EmailPasswordForm from './src/components/EmailPasswordForm';
+import LoginScreen from './src/screens/LoginScreen';
 
 import firebase from 'react-native-firebase';
 
 class App extends Component{
   render(){
     return(
-      <View style={
-        {
-          flex: 1,
-          justifyContent: 'center'
-        }
-      }>
-        <EmailPasswordForm
-          loginUser={(email,password)=>{
-            firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(()=>{
-              console.log('works')
-            })
-            .catch((error)=> {
-              // Handle Errors here.
-              var errorCode = error.code;
-              var errorMessage = error.message;
-              // ...
-              co
-            });
-          }}
-        />
+      <View>
+
       </View>
     );
   }
