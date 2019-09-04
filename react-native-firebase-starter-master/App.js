5bac1f3e99b3f7278c376506ecc3b14dae6b118f
Austin Vigo <avigo@uci.edu>
2019-09-04 10:08:21 -0700

    delete everything
    delete everything

--- react-native-firebase-starter-master/App.js
+++ react-native-firebase-starter-master/App.js
@@ -3,34 +3,12 @@ import {
   Text,
   View
 } from 'react-native';
-import EmailPasswordForm from './src/components/EmailPasswordForm';
-
-import firebase from 'react-native-firebase';
 
 class App extends Component{
   render(){
     return(
-      <View style={
-        {
-          flex: 1,
-          justifyContent: 'center'
-        }
-      }>
-        <EmailPasswordForm
-          loginUser={(email,password)=>{
-            firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(()=>{
-              console.log('works')
-            })
-            .catch((error)=> {
-              // Handle Errors here.
-              var errorCode = error.code;
-              var errorMessage = error.message;
-              // ...
-              co
-            });
-          }}
-        />
+      <View>
+
       </View>
     );
   }
