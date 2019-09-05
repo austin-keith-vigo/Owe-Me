a62017c6ffb50390118f67fb4f34eaccc881f42c
Austin Vigo <avigo@uci.edu>
2019-09-04 17:05:10 -0700

    login works with android
    login works with android

--- Records/android/app/src/main/java/com/records/MainApplication.java
+++ Records/android/app/src/main/java/com/records/MainApplication.java
@@ -13,7 +13,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 
 import java.util.List;
-
+import io.invertase.firebase.auth.RNFirebaseAuthPackage; // <-- Add this line
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -28,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+      packages.add(new RNFirebaseAuthPackage()); // <-- Add this line
       return packages;
     }
 
