b28aed402193b2364e2bce0b13340a4bd541673a
Austin Vigo <avigo@uci.edu>
2019-09-08 21:27:43 -0700

    f
    adsf

--- Records/src/screens/AddRecordScreen.js
+++ Records/src/screens/AddRecordScreen.js
@@ -1,18 +1,20 @@
 import React, {Component} from 'react';
 import {
   View,
-  Text
+  Text,
+  Button
 } from 'react-native';
 import SingletonClass from './../SingletonClass';
 import MoneyInputField from './../components/MoneyInputField';
 
 class AddRecordScreen extends Component{
 
-  state={ value: ""} 
+  state={ value: ""}
 
   render(){
     return(
       <View>
+        <Text>Enter the total amount of the Bill</Text>
         <MoneyInputField
           onChangeText={text => this.setState({value: text})}
         />
