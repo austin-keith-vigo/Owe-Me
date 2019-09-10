cebba46ecc2b09fffd6bd4437812fbe98447c2f4
Austin Vigo <avigo@uci.edu>
2019-09-10 16:29:43 -0700

    Sends a notification
    Sends notifications to all the friends

--- Records/src/FirebaseActions.js
+++ Records/src/FirebaseActions.js
@@ -20,7 +20,36 @@ const willUpdateWithNewRecord = (record) => {
     const friendsData = SingletonClass.getInstance().getFriends();
     firebase.database().ref(friendsFilepath).set(friendsData);
 
+    //Update friends firebase database
+    willUpateFriendsRecordData(record);
+
     resolve();
+  });
+}
+
+//Go through each friend in the record object and Update
+//their records with the new owed amount to the user
+const willUpateFriendsRecordData = (record) => {
+  return new Promise((resolve, reject) => {
+    var uids = {};
+
+    //Grab the users' friend's userUID's
+    firebase.database().ref('usernames').once('value').then((snapshot)=>{
+      uids = snapshot.val();
+
+      //Create and send a notification to each friend
+      for(key in record.getData()){
+        const notification = {
+          sender: SingletonClass.getInstance().getUsername(),
+          title: record.getTitle(),
+          amount: record.getAmountForPerson(key)
+        };
+
+        var filepath = uids[key] + '/notifications';
+        const newPostKey = firebase.database().ref(filepath).push().key;
+        firebase.database().ref(filepath +'/' + newPostKey).set(notification);
+      }
+    });
   });
 }
 
