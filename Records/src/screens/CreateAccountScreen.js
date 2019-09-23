7a587967ee8c2345d602a95fb616402fb99df3c4
Austin Vigo <avigo@uci.edu>
2019-09-22 20:32:39 -0700

    file was cleaned up
    file was cleaned up

--- Records/src/screens/CreateAccountScreen.js
+++ Records/src/screens/CreateAccountScreen.js
@@ -2,22 +2,18 @@ import React, {Component} from 'react';
 import {
   View,
   Text,
-  ActivityIndicator,
   StyleSheet
 } from 'react-native';
-import firebase from 'react-native-firebase';
-import Dialog from "react-native-dialog";
+
 import GLOBALS from './../Globals';
-import SingletonClass from './../SingletonClass';
-import { createAccount } from './../FirebaseActions';
+
+import { Alert, EmailUsernamePasswordForm } from './../components';
 
 import { connect } from 'react-redux';
 import {
   onCreateAccountButtonPressed,
   createAccountCloseAlert
 } from './../actions';
-
-import { Alert, EmailUsernamePasswordForm } from './../components';
 
 class CreateAccountScreen extends Component{
 
@@ -30,57 +26,9 @@ class CreateAccountScreen extends Component{
     }
   };
 
-  //Handles whether or not to render an activity monitor and alert
-  state = {creatingAccount: false, showAlert: false};
-  errorMessage="";
-
-  //Will render an Acitivity Monitor while the app is trying to login the user.
-  //Otherwise nothing will appear
-  renderActivityMonitor(){
-    if (this.state.creatingAccount == true){
-      return <ActivityIndicator size="large" color="#0000ff" />
-    }
-  }
-
-  //Will create an account in the firebase database
-  createAccountButtonPressed(email, password, username){
-    this.toggleCreateAccountState();
-
-    //Create the account with firebase
-    createAccount(email, password, username).then(()=>{
-      //Get the user's uid
-      const uid = firebase.auth().currentUser.uid;
-
-      //Set Singleton Data
-      SingletonClass.getInstance().setUserUID(uid);
-      SingletonClass.getInstance().setUsername(username);
-
-      this.toggleCreateAccountState();
-      this.props.navigation.navigate('App');
-    })
-    .catch((error) => {
-      this.toggleCreateAccountState();
-      this.showAlert(error);
-    });
-  }
-
-  //functions to toggle the states, show alert comes with a parameter
-  //becaus the parameter is the new error message
-  toggleCreateAccountState(){
-    this.setState({creatingAccount: !this.state.creatingAccount});
-  }
-
-  //Functions to handle rendering of the alert and the message
-  showAlert(message){
-    this.errorMessage = message;
-    this.setState({showAlert: true});
-  }
-  handleCloseAlert(){
-    this.setState({showAlert: false});
-  }
-
   onCreateAccountButtonPressed(){
     const { email, password, username, navigation } = this.props;
+
     this.props.onCreateAccountButtonPressed(email, username, password, navigation);
   }
 
@@ -96,7 +44,7 @@ class CreateAccountScreen extends Component{
           onPress={this.onCreateAccountButtonPressed.bind(this)}
           buttonTitle="Finish"
         />
-        {this.renderActivityMonitor()}
+
         <Alert
           isVisible={this.props.error}
           errorMessage={this.props.errorMessage}
