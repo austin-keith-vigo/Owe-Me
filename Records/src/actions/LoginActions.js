aa8d43ba5334d1265001e93241028780ea7f645f
Austin Vigo <avigo@uci.edu>
2019-09-22 18:58:49 -0700

    intializes singleton
    need to render an activity monitor

--- Records/src/actions/LoginActions.js
+++ Records/src/actions/LoginActions.js
@@ -5,7 +5,9 @@ import {
   LOGIN_USER_FAILURE,
   CLOSE_ERROR_MESSAGE
 } from './types';
+import Record from './../Record';
 import firebase from 'react-native-firebase';
+import SingletonClass from './../SingletonClass';
 
 export const emailChanged = (text) => {
   return {
@@ -21,16 +23,65 @@ export const passwordChanged = (text) => {
   };
 };
 
+//A promise that the Singleon will be initialized.
+//Is a promise to make sure the app does not move on to anymore pages until
+//Singleton is initialized.
+const willInitializeSingleton = () => {
+  return new Promise(function(resolve, reject) {
+    SingletonClass.getInstance().setUserUID(firebase.auth().currentUser.uid);
+    var filepath = SingletonClass.getInstance().getUserUID();
+    var databaseRef = firebase.database().ref(filepath);
+
+    databaseRef.once('value').then((snapshot)=>{
+
+      //Set singleton username and UID
+      SingletonClass.getInstance().setUsername(snapshot.val()['username']);
+
+      //Set SingletonRecords
+      const recordsData = snapshot.val()['records'];
+      for (var key in recordsData){
+        var newRecord = new Record(key, recordsData[key]);
+        SingletonClass.getInstance().addRecord(newRecord);
+      }
+
+      //Set Singleton friends
+      const friendsData = snapshot.val()['friends'];
+      for (var key in friendsData){
+        SingletonClass.getInstance().addFriend(key, friendsData[key]);
+      }
+
+      //Set Notifications
+      const notificationsData = snapshot.val()['notifications'];
+      for(var key in notificationsData){
+        SingletonClass.getInstance().addNotification(key, notificationsData[key]);
+      }
+
+      resolve('Completed');
+    })
+    .catch((error)=>{
+      reject(error.messsage);
+    });
+  });
+};
+
 export const loginUser = (email, password, navigation) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(() => {
-        console.log('successful login');
-        dispatch({type: LOGIN_USER_SUCCESS});
-        navigation.navigate('App');
+        //Initialize the Singleton before moving on
+        willInitializeSingleton()
+          .then(()=> {
+            dispatch({type: LOGIN_USER_SUCCESS});
+            navigation.navigate('App');
+          })
+          .catch((error)=> {
+            dispatch({
+              type: LOGIN_USER_FAILURE,
+              payload: error.message
+            });
+          });
       })
       .catch((error) => {
-        console.log(error.message);
         dispatch({
           type: LOGIN_USER_FAILURE,
           payload: error.message
--- Records/src/screens/LoginScreen.js
+++ Records/src/screens/LoginScreen.js
@@ -30,90 +30,6 @@ class LoginScreen extends Component{
     }
   };
 
-  //Handles whether or not to render an activity monitor and alert
-  state = {loggingIn: false, showAlert: false};
-  errorMessage = "";
-
-  //Logs in the user using firebase's authentication.
-  //Also, handles turning off and on the activity monitor
-  loginUser(email,password){
-    this.toggleLoggingInState();
-    firebase.auth().signInWithEmailAndPassword(email, password).then(()=>{
-      this.toggleLoggingInState();
-      this.willInitializeSingleton().then(()=>{
-        this.props.navigation.navigate('App');
-        // this.props.navigation.dispatch(resetAction);
-      })
-      .catch((message)=>{
-        this.errorMessage = message;
-        this.setState({loggingIn: false, showAlert: true})
-      });
-    })
-    .catch((error)=>{
-      this.errorMessage = error.message;
-      this.setState({loggingIn: false, showAlert: true})
-    });
-  }
-
-  //A promise that the Singleon will be initialized.
-  //Is a promise to make sure the app does not move on to anymore pages until
-  //Singleton is initialized.
-  willInitializeSingleton(){
-    return new Promise(function(resolve, reject) {
-      SingletonClass.getInstance().setUserUID(firebase.auth().currentUser.uid);
-      var filepath = SingletonClass.getInstance().getUserUID();
-      var databaseRef = firebase.database().ref(filepath);
-
-      databaseRef.once('value').then((snapshot)=>{
-
-        //Set singleton username and UID
-        SingletonClass.getInstance().setUsername(snapshot.val()['username']);
-
-        //Set SingletonRecords
-        const recordsData = snapshot.val()['records'];
-        for (var key in recordsData){
-          var newRecord = new Record(key, recordsData[key]);
-          SingletonClass.getInstance().addRecord(newRecord);
-        }
-
-        //Set Singleton friends
-        const friendsData = snapshot.val()['friends'];
-        for (var key in friendsData){
-          SingletonClass.getInstance().addFriend(key, friendsData[key]);
-        }
-
-        //Set Notifications
-        const notificationsData = snapshot.val()['notifications'];
-        for(var key in notificationsData){
-          SingletonClass.getInstance().addNotification(key, notificationsData[key]);
-        }
-
-        resolve('Completed');
-      })
-      .catch((error)=>{
-        reject(error.messsage);
-      });
-    });
-  }
-
-  //Will render an Acitivity Monitor while the app is trying to login the user.
-  //Otherwise nothing will appear
-  renderActivityMonitor(){
-    if (this.state.loggingIn == true){
-      return <ActivityIndicator size="large" color="#0000ff" />
-    }
-  }
-
-  //Toggles the state of logginIn so I don't have to keep calling setState();
-  toggleLoggingInState(){
-    this.setState({loggingIn: !this.state.loggingIn});
-  }
-
-  //Toggles the state of showAlert so I don't have to keep calling setState();
-  toggleShowAlertState(){
-    this.setState({showAlert: !this.state.showAlert});
-  }
-
   //Users an action to handle user login
   loginButtonPressed(){
     const { email, password, navigation } = this.props;
@@ -121,16 +37,17 @@ class LoginScreen extends Component{
     this.props.loginUser(email, password, navigation);
   }
 
+  //closes the dialog component
   closeAlert() {
     this.props.closeErrorMessage();
   }
+
 
   render(){
     return(
       <View style={styles.viewStyle}>
         <Text style={styles.title}>RECORDS</Text>
         <EmailPasswordForm
-          buttonPressed={this.loginUser.bind(this)}
           buttonTitle="Login"
           loginButtonPressed={this.loginButtonPressed.bind(this)}
         />
@@ -141,7 +58,6 @@ class LoginScreen extends Component{
             onPress={this.closeAlert.bind(this)}
           />
         </Dialog.Container>
-        {this.renderActivityMonitor()}
         <TouchableOpacity onPress={()=>{
           this.props.navigation.navigate('ForgotPassword');
         }}>
